264d254ce861d4c5ef21b31fcd9cc8b37183e317
Avishek Kumar <kumaravishek84@gmail.com>
2024-08-16 01:35:08 +0530

    muntithreading with ReentrantLock

--- src/AdderSubtractor/Client.java
+++ src/AdderSubtractor/Client.java
@@ -2,15 +2,18 @@ package AdderSubtractor;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Client {
     public static void main(String[] args) {
         Value v = new Value(0);
+        Lock lock = new ReentrantLock();
 
         ExecutorService es = Executors.newFixedThreadPool(5);
 
-        Adder adder = new Adder(v);
-        Subtractor subtractor = new Subtractor(v);
+        Adder adder = new Adder(v, lock);
+        Subtractor subtractor = new Subtractor(v, lock);
 
         es.submit(adder);
         es.submit(subtractor);
--- src/AdderSubtractor/Subtractor.java
+++ src/AdderSubtractor/Subtractor.java
@@ -1,19 +1,23 @@
 package AdderSubtractor;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.locks.Lock;
 
 public class Subtractor implements Callable<Void> {
     Value value;
-
-    Subtractor(Value value) {
+    Lock lock;
+    Subtractor(Value value, Lock lock) {
         this.value = value;
+        this.lock = lock;
     }
 
     public Void call(){
         for(int i = 0; i <= 100; i++){
+            lock.lock();
             System.out.print(this.value.val + " - " + i + " = ");
             this.value.val -= i;
             System.out.println(this.value.val + " Pool ---> " + Thread.currentThread().getName());
+            lock.unlock();
         }
         return null;
     }
