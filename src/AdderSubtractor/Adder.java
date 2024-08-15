e0b1808c30f3df215811b24e358f2a80f7e737be
Avishek Kumar <kumaravishek84@gmail.com>
2024-08-16 01:42:36 +0530

    multithreading with synchronized lock

--- src/AdderSubtractor/Adder.java
+++ src/AdderSubtractor/Adder.java
@@ -5,19 +5,17 @@ import java.util.concurrent.locks.Lock;
 
 public class Adder implements Callable<Void> {
     Value value;
-    Lock lock;
-    Adder(Value value, Lock lock) {
+    Adder(Value value) {
         this.value = value;
-        this.lock = lock;
     }
 
     public Void call(){
         for(int i = 0; i <= 100; i++){
-            lock.lock();
-            System.out.print(this.value.val + " + " + i + " = ");
-            this.value.val += i;
-            System.out.println(this.value.val + " Pool ---> " + Thread.currentThread().getName());
-            lock.unlock();
+            synchronized (value) {
+                System.out.print(this.value.val + " + " + i + " = ");
+                this.value.val += i;
+                System.out.println(this.value.val + " Pool ---> " + Thread.currentThread().getName());
+            }
         }
         return null;
     }
--- src/AdderSubtractor/Client.java
+++ src/AdderSubtractor/Client.java
@@ -8,12 +8,11 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Client {
     public static void main(String[] args) {
         Value v = new Value(0);
-        Lock lock = new ReentrantLock();
 
         ExecutorService es = Executors.newFixedThreadPool(5);
 
-        Adder adder = new Adder(v, lock);
-        Subtractor subtractor = new Subtractor(v, lock);
+        Adder adder = new Adder(v);
+        Subtractor subtractor = new Subtractor(v);
 
         es.submit(adder);
         es.submit(subtractor);
