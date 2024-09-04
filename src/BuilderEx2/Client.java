0dbf59a77372f73fe526a38b46ec89d41c60f79d
Avishek Kumar <kumaravishek84@gmail.com>
2024-09-05 01:00:05 +0530

    issue with V0

--- src/BuilderEx2/Client.java
+++ src/BuilderEx2/Client.java
@@ -9,5 +9,12 @@ public class Client {
                 .setGender("M")
                 .setBatch("XYASA")
                 .build();
+        // Still with the current implementation it is possible that some one created a builder object
+        // normally and created Student object with that without having any validation since validation is
+        // inside build method.
+
+        // Above problem occured as constuctor is public
+        // But if we make constructor private then inside build method of StudentBuilder class also. We wo't be
+        // able to create a object of Student.
     }
 }
