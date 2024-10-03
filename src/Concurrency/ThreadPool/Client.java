61da6b6a4b311ba8d1aa57164a7ca853a2cb3d44
Avishek Kumar <kumaravishek84@gmail.com>
2024-10-03 21:13:38 +0530

    Created cached thread pool

--- src/Concurrency/ThreadPool/Client.java
+++ src/Concurrency/ThreadPool/Client.java
@@ -5,7 +5,8 @@ import java.util.concurrent.Executors;
 
 public class Client {
     public static void main(String[] args) {
-        ExecutorService es = Executors.newFixedThreadPool(8);
+        // Cached Thread pool creates new thread if all other thread are busy executing task.
+        ExecutorService es = Executors.newCachedThreadPool();
 
         for(int i = 1; i <= 10000; i++){
             NumberPrinter np = new NumberPrinter(i);
